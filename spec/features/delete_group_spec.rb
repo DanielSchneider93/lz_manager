67cbaa4ff879f9a604adfeae96d22e2fcf30b314
Daniel Schneider <33485536+DanielSchneider93@users.noreply.github.com>
2017-12-06 13:27:30 +0100

    rename tests to test_spec, fixed links

--- spec/features/delete_group_spec.rb
+++ spec/features/delete_group_spec.rb
@@ -8,12 +8,12 @@ describe Group do
   end
 
   it 'delete Group' do
-    
+
     puts "\n Click delete Group Link"
     visit root_path
     click_on 'delete_group'
 
     puts "\n Check if Group is deleted"
-    expect(page).to_ have_content(@group.title)
+    expect(page).to_not have_content('Test')
   end
 end
